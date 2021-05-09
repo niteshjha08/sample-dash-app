1783664039df8a64069da95b8c06271afb0764b4
Nitesh Jha <niteshj818@gmail.com>
2021-05-09 23:56:55 +0530

    Added initial files

---
+++ dash01.py
@@ -0,0 +1,21 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import numpy as np
+print("hello world")
+app=dash.Dash()
+app.layout=html.Div([
+    html.H1("Hey there!!"),
+    html.Div("This is the dash tutorial"),
+    dcc.Graph(
+        id='sample1',
+        figure={
+            'data':[{'x':[1,2,3],'y':[7,8,9],'type':'bar','name':'NJ'},
+                    {'x':[5,2,3],'y':[17,18,19],'type':'bar','name':'NJ2'}],
+            'layout':{
+                'title':'Simple Bar Chart',
+                'plot_bgcolor':'#D3D3D3'
+            }}
+    )])
+app.run_server()
