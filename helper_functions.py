0c4313c71bfd8e95cb67049cec117fd98268c87c
Nitesh Jha <niteshj818@gmail.com>
2021-05-13 19:35:13 +0530

    random

--- helper_functions.py
+++ helper_functions.py
@@ -23,3 +23,12 @@ def _build_auth_code_flow(authority=None, scopes=None):
     return _build_msal_app(authority=authority).initiate_auth_code_flow(
         scopes or [],
         redirect_uri=url_for("authorized", _external=True))
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache=cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
